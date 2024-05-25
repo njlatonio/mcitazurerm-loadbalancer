779dbf09633251d671526f85970b622a479cd88e
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-24 20:00:39 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -11,8 +11,9 @@ variable "tenant_id"{
   type=string
 }
 
-variable "resource_group_name"{
-  type        = string
+
+variable "resource_group_name" {
+  type=string
   description = "(Required) The name of the resource group where the load balancer resources will be imported."
 }
 
