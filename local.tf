652b791a038277d6e041cc874898c371f00db711
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-24 20:39:13 -0400

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,7 +1,6 @@
-/*
 locals {
   lb_name   = var.name != "" ? var.name : format("%s-lb", var.prefix)
   pip_name  = var.pip_name != "" ? var.pip_name : format("%s-publicIP", var.prefix)
   subnet_id = try(coalesce(local.data_subnet_id, var.frontend_subnet_id), null)
 }
-*/
+
