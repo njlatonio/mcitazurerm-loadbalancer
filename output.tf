998c80bfbf9cf30d891591c41665135dbbb1f7bb
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-24 20:27:33 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+/*
 output "azurerm_lb_backend_address_pool_id" {
   description = "the id for the azurerm_lb_backend_address_pool resource"
   value       = azurerm_lb_backend_address_pool.azlb.id
@@ -42,3 +43,4 @@ output "azurerm_resource_group_tags" {
   description = "the tags provided for the resource group"
   value       = data.azurerm_resource_group.azlb.tags
 }
+*/
